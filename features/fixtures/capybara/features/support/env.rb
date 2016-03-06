d9c6a2eb7c2e46743481d9736c4db4a9194a070d
Jason Karns <jason@karns.name>
2016-03-06 17:05:05 -0500

    configure capybara-webkit to mute some warnings

--- features/fixtures/capybara/features/support/env.rb
+++ features/fixtures/capybara/features/support/env.rb
@@ -1,6 +1,11 @@
 require 'capybara/cucumber'
-require 'capybara-webkit'
 Capybara.default_driver = :webkit
+
+require 'capybara-webkit'
+Capybara::Webkit.configure do |config|
+  config.block_unknown_urls
+  config.allow_url("abcdcomputech.dequecloud.com")
+end
 
 # to use out of the box step definitions
 require 'axe/cucumber/step_definitions'
