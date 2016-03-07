4e8a07298b892c1542c17f8fae8745416e6f679d
Jason Karns <jason@karns.name>
2016-03-06 21:19:01 -0500

    Restore cucumber profiles and carry driver through to fixture

--- features/fixtures/capybara/features/support/env.rb
+++ features/fixtures/capybara/features/support/env.rb
@@ -1,11 +1,24 @@
 require 'capybara/cucumber'
-Capybara.default_driver = :webkit
-
-require 'capybara-webkit'
-Capybara::Webkit.configure do |config|
-  config.block_unknown_urls
-  config.allow_url("abcdcomputech.dequecloud.com")
-end
 
 # to use out of the box step definitions
 require 'axe/cucumber/step_definitions'
+
+# load and select the appropriate browser
+Capybara.default_driver = case ENV['BROWSER'].to_sym
+                          when :webkit
+                            require 'capybara-webkit'
+                            Capybara::Webkit.configure do |config|
+                              config.block_unknown_urls
+                              config.allow_url("abcdcomputech.dequecloud.com")
+                            end
+                            :webkit
+                          when :poltergeist
+                            require 'capybara/poltergeist'
+                            :poltergeist
+                          else
+                            require 'selenium-webdriver'
+                            Capybara.register_driver :selenium do |app|
+                              Capybara::Selenium::Driver.new(app, :browser => ENV['BROWSER'].to_sym)
+                            end
+                            :selenium
+                          end
